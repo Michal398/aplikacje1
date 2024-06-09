0555f949d9051021868ed3763360888983d8ed28
Michal398 <112950481+Michal398@users.noreply.github.com>
2024-06-09 14:31:25 +0200

    Add files via upload

---
+++ Zadanie2desktop/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie2desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                pictureBox1.Image = Properties.Resources.pocztowka;
+                label1.Text = "Cena: 1zł";
+            }else if(radioButton2.Checked)
+            {
+                pictureBox1.Image = Properties.Resources.list;
+                label1.Text = "Cena: 1,5zł";
+            }
+            else if (radioButton3.Checked)
+            {
+                pictureBox1.Image = Properties.Resources.paczka;
+                label1.Text = "Cena: 10zł";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int test;
+            bool ok = int.TryParse(textBox2.Text, out test);
+            if (textBox2.Text.Length != 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+            }
+            else if (!ok)
+            {
+                MessageBox.Show("Kod pocztowy powinien składać się z samych cyfr");
+            }
+            else
+            {
+                MessageBox.Show("Dane przesyłko zostały wprowadzone");
+            }
+        }
+    }
+}
---
+++ Zadanie2desktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadanie2desktop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie2desktop")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("35f13f73-92d5-43d5-83f4-d7060e1abffa")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
